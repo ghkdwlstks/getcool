a1d6121cb31ac18664510d0973fd64558556cbb2
tlgusyu <tlgusyu@kyonggi.ac.kr>
2024-11-02 21:35:41 +0900

    Update delete query(integration).sql

--- 통합 DB/delete query(integration).sql
+++ 통합 DB/delete query(integration).sql
@@ -1,4 +1,5 @@
 -- 2024. 10. 16 integration
+-- 2024. 11. 02 last updates
 -- SQL delete
 
 # edit_Minfo.html, edit_info.html
@@ -10,7 +11,7 @@ WHERE user_id = ?;
 # wishlist.html
 # 장바구니 제거
 DELETE FROM cart
-WHERE user_id = ? AND product_id = ?;
+WHERE buyer_id = ? AND product_id = ?;
 	-- ? : Nods.js의 검색어 들어가는 자리
     
 -- 특정 주문 상세 정보 삭제 (예: order_detail_id가 1인 경우)
@@ -27,7 +28,7 @@ WHERE order_id = 1;
 
 -- 특정 사용자의 모든 주문 정보 삭제 (예: user_id가 1인 경우)
 DELETE FROM `order`
-WHERE user_id = 1;
+WHERE buyer_id = 1;
 
 -- 특정 사용자 정보 삭제 (예: user_id가 1인 경우)
 DELETE FROM user
